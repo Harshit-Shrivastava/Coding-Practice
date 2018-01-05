696f115f7dc4784065a4953a7b265bb07c14fe4b
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2018-01-05 16:29:37 +0530

    Solution to find smallest missing value in constant space and time

---
+++ Questions/Find missing value in constant space and time/Solution.java
@@ -0,0 +1,36 @@
+public class Solution{
+	public static void main(String args[]){
+		int[] arr = new int[]{3, -2, 0, 4};
+		int index = partition(arr);
+		System.out.println("Missing value: " + missing(arr, index));
+		for(int i = 0; i < arr.length; i++){
+			System.out.print(arr[i] + "\t");
+		}
+	}
+	
+	public static int partition(int arr[]){
+		int j = 0;
+		for(int i = 0; i < arr.length; i++){
+			if(arr[i] <= 0){
+				int temp = arr[i];
+				arr[i] = arr[j];
+				j++;
+			}
+		}
+		return j;
+	}
+	
+	public static int missing(int arr[], int index){
+		for(int i = 0; i < arr.length - index; i++){
+			if(Math.abs(arr[i]) -1 < (arr.length - index) && arr[Math.abs(arr[i] - 1)] > 0){
+				arr[Math.abs(arr[i]) - 1] = -arr[Math.abs(arr[i]) - 1];
+			}
+		}
+		for(int i = 0; i < arr.length - index; i++){
+			if(arr[i] > 0){
+				return i+1;
+			}
+		}
+		return arr.length - index;
+	}
+}
