b9085fc691fcb69a0c1e8688871e46218f0642e8
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2018-01-05 16:47:14 +0530

    Bug fix

--- Questions/Find missing value in constant space and time/Solution.java
+++ Questions/Find missing value in constant space and time/Solution.java
@@ -1,6 +1,6 @@
 public class Solution{
 	public static void main(String args[]){
-		int[] arr = new int[]{3, -2, 0, 4};
+		int[] arr = new int[]{2, 3, 4, 5, 5, 1, -7, 8};
 		int index = partition(arr);
 		System.out.println("Missing value: " + missing(arr, index));
 		for(int i = 0; i < arr.length; i++){
@@ -22,7 +22,7 @@ public class Solution{
 	
 	public static int missing(int arr[], int index){
 		for(int i = 0; i < arr.length - index; i++){
-			if(Math.abs(arr[i]) -1 < (arr.length - index) && arr[Math.abs(arr[i] - 1)] > 0){
+			if(Math.abs(arr[i]) -1 < (arr.length - index) && arr[Math.abs(arr[i]) - 1] > 0){
 				arr[Math.abs(arr[i]) - 1] = -arr[Math.abs(arr[i]) - 1];
 			}
 		}
